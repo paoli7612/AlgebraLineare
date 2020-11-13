4d0563bb0f5f2f64dbf60dd41d3843f3a6842a03
paoli7612 <paoli7612@gmail.com>
2020-11-13 04:41:26 +0100

    complex

--- numeri complessi/complex.hpp
+++ numeri complessi/complex.hpp
@@ -15,9 +15,28 @@ Complex new_complex(float real, float immaginary){
 }
 
 void print_complex(Complex c){
-    cout << c.real << " ";
-    if (c.immaginary > 0)
-        cout << "+";
-    if (c.immaginary)
-        cout << c.immaginary << "i";
+    cout << c.real << " + " << c.immaginary << "i";
+}
+
+Complex coniugato(Complex a){
+    Complex c;
+    c.real = a.real;
+    c.immaginary = -a.immaginary;
+    return c;
+}
+
+Complex somma(Complex a, Complex b){
+    Complex c;
+    c.real = a.real + b.real;
+    c.immaginary = a.immaginary + b.immaginary;
+    return c;
+}
+
+Complex prodotto(Complex a, Complex b){
+    Complex c;
+
+    c.immaginary = (a.real*b.real) - (a.immaginary*b.immaginary);
+    c.real = (a.real*b.immaginary) + (a.immaginary*b.real);
+
+    return c;
 }
--- numeri complessi/main.cc
+++ numeri complessi/main.cc
@@ -6,12 +6,26 @@ using namespace std;
 int main(int argc, char** argv){
 
     Complex a = new_complex(2, 3);
-    Complex b = new_complex(6, -2);
 
     print_complex(a);
     cout << endl;
-    print_complex(b);
+     
+    print_complex(coniugato(a));
     cout << endl;
+
+    print_complex(somma(a, coniugato(a)));
+    cout << endl;
+
+    print_complex(prodotto(a, coniugato(a)));
+    cout << endl;
+
+    cout << "radice di meno uno" << endl;
+    Complex u = new_complex(1, 0);
+    print_complex(u);
+    cout << " * ";
+    print_complex(u);
+    cout << endl;
+    print_complex(prodotto(u, u));
 
     return 0;
 }
