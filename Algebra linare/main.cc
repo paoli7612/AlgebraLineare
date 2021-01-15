7a5188894e9604213ebeab7d5c9cfc6340a7bac7
Tommaso Paoli <paoli7612@gmail.com>
2021-01-15 20:07:12 +0100

    test

--- Algebra linare/main.cc
+++ Algebra linare/main.cc
@@ -6,74 +6,37 @@
 using namespace std;
 
 struct matrice_t {
+	int r, c;
 	int** v;
-	int r, c; // righe colonne
 };
 
-struct matrici_t {
-	matrice_t* mm;
-	int el, N;
+struct insieme_t {
+	int N;
+	matrice_t* matrici;
+	int el;
 };
-void stampa_matrice(const matrice_t m){
-	for (int y=0; y<m.r; y++){
-		for (int x=0; x<m.c; x++)
-			cout << m.v[y][x] << " ";
-		cout << endl;
-	} cout << endl;
-}
 
-void stampa(const struct matrici_t matrici){
-	for (int i=0; i<matrici.el; i++)
-		stampa_matrice(matrici.mm[i]);
-}
-
-void inizializza_matrice(matrice_t &m, int r, int c){
-	m.r = r;
-	m.c = c;
-	m.v = new int*[c];
-	for (int y=0; y<m.r; y++){
-		m.v[y] = new int[r];
-		for (int x=0; x<m.c; x++)
-			m.v[y][x] = 0;
-	}
+void init(struct insieme_t insieme, int n){
+	insieme.matrici = new matrice_t[n];
+	insieme.N = n;
+	insieme.el = 0;
 }
 
 int main(){
-	matrici_t matrici;
 	
-	const char menu[] =
-		"\t1. Inizializza lista matrici\n"
-		"\t2. Stampa matrici\n"
-		"\t3. Aggiungi matrice\n";
+	insieme_t insieme;
+	init(insieme, 4);
 	
-	while (true){
-		int scelta;
-		cout << menu << endl;
-		cout << "Scelta: "; 
-		cin >> scelta;
+	const char menu[] = 
+		"1. Crea matrice\n"
+		"2. Stampa insieme\n"
+		"3. Rimuovi prima matrice\n";
 		
-		switch (scelta){
-			int n;
-			case 1:
-				cout << "numero massimo matrici: ";
-				do {
-					cin >> n;
-					matrici.mm = new matrice_t[n];
-				} while (n < 0 || n > 10000);
-				matrici.N = n;
-				matrici.el = 0;
-				break;
-			case 2:
-				stampa(matrici);
-				break;
-			case 3: {
-				int r, c;
-				cout << "inserisci n_righe e n_colonne: ";
-				cin >> r >> c;
-				inizializza_matrice(matrici.mm[matrici.el++], r, c);
-				break;
-			}
-		}
-	}	
+		
+	bool run;
+	while (run){
+		
+	}
+	
 	
 }
