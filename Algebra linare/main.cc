d85684eb1521c0d57394d914df017b95db56d2e7
Tommaso Paoli <paoli7612@gmail.com>
2021-01-17 10:42:25 +0100

    un po all abvolta

--- Algebra linare/main.cc
+++ Algebra linare/main.cc
@@ -5,21 +5,54 @@
 
 using namespace std;
 
+
+// Descirttore matrice
 struct matrice_t {
-	int r, c;
-	int** v;
+	int righe, colonne;
+	int** valori;
 };
 
+// Descrittore insieme di matrici
 struct insieme_t {
-	int N;
+	int N;					// Max elementi
 	matrice_t* matrici;
-	int el;
+	int elementi;
 };
 
-void init(struct insieme_t insieme, int n){
+// Inizializza insieme vuoto a max n matrici
+void init(struct insieme_t &insieme, int n)
+{
 	insieme.matrici = new matrice_t[n];
 	insieme.N = n;
-	insieme.el = 0;
+	insieme.elementi = 0;
+}
+
+stampa_matrice(const struct matrice_t &matrice)
+{
+	for (int y=0; y<matrice.righe; y++)
+		for (int x=0; x<matrice.colonne; y++)
+			matrice.valori[y][x] = 0;
+}
+
+void stampa_insieme(const struct insieme_t &insieme)
+{
+	for (int i=0; i<insieme.elementi; i++)
+		stampa_matrice(insieme.matrici[i]);
+}
+
+// Chiede le dimensioni di una nuova matrice e la aggiunge all'insieme
+void crea_matrice(struct insieme_t &insieme){
+	matrice_t *matrice = insieme.matrici[insieme.elementi++];
+	
+	cout << "numero righe: ";
+	cin >> matrice->righe;
+	cout << "numero colonne: ";
+	cin >> matrice->colonne;
+	
+	matrice->valori = new int*[];
+	for (int y=0; y<matrice->righe; y++)
+		for (int x=0; x<matrice->colonne; y++)
+			matrice->valori[y][x] = 0;
 }
 
 int main(){
